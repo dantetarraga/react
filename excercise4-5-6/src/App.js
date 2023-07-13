0bff767cf9d6cd0c69893d36d5fe8ee1b20a45dc
dantetarraga <dante.tarragau@gmail.com>
2023-07-13 14:38:13 -0500

    Excersice 4-5-6

---
+++ excercise4-5-6/src/App.js
@@ -0,0 +1,12 @@
+import MyApp from './components/Clock';
+import Clock from './components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <MyApp></MyApp>
+    </div >
+  );
+}
+
+export default App;
---
+++ excercise4-5-6/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    boxShadow: {
+      'custom': '0 2px 30px rgba(0, 0, 0, 0.1)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
